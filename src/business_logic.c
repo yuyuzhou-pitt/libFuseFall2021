43d83c940acd9d4fdde66cbb2995c036570202cc
Jack Ullery <jmu17@pitt.edu>
2021-03-10 15:15:38 -0500

    most recent changes

--- src/business_logic.c
+++ src/business_logic.c
@@ -3,21 +3,24 @@
 #include "database_interface.h"
 
 int check_action (const char *user, int size_change){
-	Record *record;
+	Record record;
 	
 	if (get_user_record(user, &record) != 0)
 		return 1;
 	
-	if ((record->total + size_change) <= record->quota)
+	if ((record.total + size_change) <= record.quota)
 		return 1;
 	else 
 		return 0;
 }
 
 int update_user_total (const char *user, int total_change){
-	if(change_user_record(user, total_change, 0) == 0){
+	Record record;
+	if(get_user_record(user, &record)){
+		return add_record(user, total_change, DEFAULT_QUOTA);
+	}
+	else if(change_user_record(user, total_change, 0) == 0){
 		return 0;
 	}
 
-	return add_record(user, total_change, DEFAULT_QUOTA);
 }
--- src/database_interface.c
+++ src/database_interface.c
@@ -96,7 +96,7 @@ int change_databaseFile(const char *file_name)
 int find_block(const char *user, FILE *fp)
 {
 	uint32_t i = 0;
-	char 	 buffer[BLOCK_SIZE];
+	char 	 buffer[BLOCK_SIZE+1];
 	char 	*temp;
 	printf("Username to find: %s\n", user);
 	while (fgets(buffer, BLOCK_SIZE+1, fp) != NULL){
