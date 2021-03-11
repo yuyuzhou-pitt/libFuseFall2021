a1ef426d20303e071c411a1db00841615cbce349
Jack Ullery <jmu17@pitt.edu>
2021-03-10 23:12:16 -0500

    cleaning code saves lives

--- src/common.h
+++ src/common.h
@@ -2,6 +2,8 @@
 #define _COMMON_H_
 
 #include <stdint.h>
+#include <stdarg.h>
+
 
 //// Default quota size is 1 MB
 // #define DEFAULT_USAGE_QUOTA 1048576
@@ -9,5 +11,7 @@
 // The default user can create 1000 files
 #define DEFAULT_FILE_QUOTA 1000
 
+// Printf to log file. Implemented in ntapfuse_ops.c
+void log_data(const char * format, ...);
 
 #endif /*_COMMON_H_*/
--- src/database.c
+++ src/database.c
@@ -7,48 +7,19 @@
 
 #include <limits.h> 
 #include <fuse.h> 
-#include <stdarg.h>
 #include <string.h>
 
 #include <stdint.h>
 
-void
-fullpath2 (const char *path, char *buf)
-{
-  char *basedir = (char *) fuse_get_context ()->private_data;
-
-  strcpy (buf, basedir);
-  strcat (buf, path);
-}
-
-void
-log_data2(const char * format, ...){
-    
-  char fpath[1000];
-  const char* lpath = "/../log.txt";
-  fullpath2 (lpath, fpath);
-  FILE * fp = fopen(fpath, "a");    
-
-  va_list args;
-  va_start(args, format);
-  vfprintf(fp, format, args);
-  va_end(args);
-
-  fclose(fp);
-}
-
 // The database we do all the work on.
 sqlite3 *db;
 
-/*
- * Returns a user record
- * returns 1 if successful, 0 if unsuccessful 
- */
+// See documentation in database.h
 int contains_user(uint64_t user_id){
     sqlite3_stmt *stmt;
     int re = sqlite3_prepare_v2(db, "SELECT user_id FROM USER_QUOTAS WHERE user_id == ?", -1, &stmt, 0);    
     if (re != SQLITE_OK) {
-        fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
+        log_data("Failed to fetch data: %s\n", sqlite3_errmsg(db));
         return 1;
     }    
     
@@ -59,18 +30,12 @@ int contains_user(uint64_t user_id){
     return re == SQLITE_ROW;
 }
 
-/*
- * Changes the usage total for given user
- * uint64_t user_id
- * int32_t total_change: total number of bytes to add to usage
- * returns 1 if successful, 0 if unsuccessful
- */
+// See documentation in database.h
 int add_usage_record(uint64_t user_id, int32_t total_change){
-    log_data2("ADDING USAGE\n");
     sqlite3_stmt *stmt;
     int re = sqlite3_prepare_v2(db, "UPDATE USER_QUOTAS SET usage_total = usage_total + ? WHERE user_id = ?", -1, &stmt, 0);    
     if (re != SQLITE_OK) {
-        log_data2("Failed to fetch data (%d): %s\n", re, sqlite3_errmsg(db));
+        log_data("Failed to fetch data (%d): %s\n", re, sqlite3_errmsg(db));
         return 1;
     }    
     
@@ -80,24 +45,19 @@ int add_usage_record(uint64_t user_id, int32_t total_change){
     sqlite3_finalize(stmt);
 
     if (re != SQLITE_DONE) {
-        log_data2("Failed to fetch data (%d): %s\n", re, sqlite3_errmsg(db));
+        log_data("Failed to fetch data (%d): %s\n", re, sqlite3_errmsg(db));
     }
 
     print_all();
     return re == SQLITE_DONE;
 }
 
-/*
- * Changes the file total for given user
- * uint64_t user_id
- * int32_t total_change: total number of files to add to the record
- * returns 1 if successful, 0 if unsuccessful
- */
+// See documentation in database.h
 int add_file_record(uint64_t user_id, int32_t total_change){
     sqlite3_stmt *stmt;
     int re = sqlite3_prepare_v2(db, "UPDATE USER_QUOTAS SET file_total = file_total + ? WHERE user_id = ?", -1, &stmt, 0);    
     if (re != SQLITE_OK) {
-        fprintf(stderr, "Failed to fetch data (%d): %s\n", re, sqlite3_errmsg(db));
+        log_data("Failed to fetch data (%d): %s\n", re, sqlite3_errmsg(db));
         return 1;
     }    
     
@@ -107,11 +67,12 @@ int add_file_record(uint64_t user_id, int32_t total_change){
     sqlite3_finalize(stmt);
 
     if (re != SQLITE_DONE) {
-        fprintf(stderr, "Failed to fetch data (%d): %s\n", re, sqlite3_errmsg(db));
+        log_data("Failed to fetch data (%d): %s\n", re, sqlite3_errmsg(db));
     }
     return re == SQLITE_DONE;
 }
 
+// See documentation in database.h
 void clear_db(){
     const char *sql_stmt = "DELETE FROM USER_QUOTAS";
     char *error_message;
@@ -119,44 +80,39 @@ void clear_db(){
 
     int re = sqlite3_exec(db, sql_stmt, NULL, 0, &error_message);
     if(re !=  SQLITE_OK){
-        printf("Error: %s\n",error_message);
+        log_data("Error: %s\n",error_message);
     }
 }
 
+//Helper function for print_all()
 int print_row(void *unused, int argc, char **argv, char **col_name) {    
     (void*)unused;
     
     for (int i = 0; i < argc; i++) {
-        log_data2("%2s |\t", argv[i]);
+        log_data("%s:%4s\t|  ", col_name[i], argv[i]);
     }
-    log_data2("\n");
+    log_data("\n");
     
     return 0;
 }
 
-void 
-print_all(){
-    log_data2("Printing everything.\n");
+// See documentation in database.h
+void print_all(){
+    log_data("Printing everything in table:\n");
     const char *sql_stmt = "SELECT * FROM USER_QUOTAS";
     char *error_message;
     sqlite3_stmt *stmt;
 
     int re = sqlite3_exec(db, sql_stmt, print_row, 0, &error_message);
     if(re !=  SQLITE_OK){
-        log_data2("Error: %s\n",error_message);
+        log_data("Error: %s\n",error_message);
         return;
     }
 }
 
-/*
- * Adds a record to the database
- * const char *user: string name of user
- * int64_t total: total bytes used by user
- * int64_t quota: quota of user
- * returns 0 if successful, corresponsing SQLite error code if unsuccessful
- */
+// See documentation in database.h
 int add_user(uint64_t user_id){
-    fprintf(stderr, "Adding user %lu\n", user_id);
+    log_data("Adding user %lu into table\n", user_id);
     const char *sql_stmt = "INSERT INTO USER_QUOTAS(user_id, usage_quota, file_quota) VALUES (?, ?, ?)";
     sqlite3_stmt *stmt;
     int re = sqlite3_prepare_v2(db, sql_stmt, -1, &stmt, NULL);
@@ -178,11 +134,11 @@ int add_user(uint64_t user_id){
     return 0;
 }
 
-int
-db_init(const char* path){
+// See documentation in database.h
+int db_init(const char* path){
    int re = sqlite3_open_v2(path, &db, SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE, NULL);
    if(re) {
-      log_data2("Can't open database (%d): %s\n", re, sqlite3_errmsg(db));
+      log_data("Can't open database (%d): %s\n", re, sqlite3_errmsg(db));
       return 0;
    } 
 
@@ -206,39 +162,18 @@ db_init(const char* path){
     char *error_message;
     re = sqlite3_exec(db, sql, 0, 0, &error_message);
     if(re != SQLITE_OK){
-      log_data2("Can't inialize database (%d): %s\n", re, sqlite3_errmsg(db));
+      log_data("Can't inialize database (%d): %s\n", re, sqlite3_errmsg(db));
       return 0;
     }
 
-    log_data2("Initalized\n");
+    log_data("Initalized\n");
     return 1;
 }
 
-int
-db_close(){
+// See documentation in database.h
+int db_close(){
     int re = sqlite3_close(db);
     db = NULL; 
-    log_data2("Closed\n");
+    log_data("Closed\n");
     return re;
 }
-
-// int main(int argc, char* argv[]) {
-//     db_init("test.db");
-//     // Some tests to make sure the database works
-//     fprintf(stderr, "Opened database successfully\n");
-//     add_user(1);
-//     add_user(4);
-//     print_all();
-//     add_usage_record(1, 5);
-//     add_usage_record(1, 1000000000); // This should fail
-//     print_all();
-
-//     printf("C: %s \n", contains_user(0)? "true" : "false");
-//     printf("C: %s \n", contains_user(1)? "true" : "false");
-//     printf("C: %s \n", contains_user(2)? "true" : "false");
-//     printf("C: %s \n", contains_user(3)? "true" : "false");
-//     printf("C: %s \n", contains_user(4)? "true" : "false");
-//     clear_db();
-//     print_all();
-//     db_close();
-// }
