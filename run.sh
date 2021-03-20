381cdd24532e8ef7b77a4d28ebe4e7546c541683
bricktsre <gwoodward21@gmail.com>
2021-03-20 16:41:29 -0400

    Removing and adding files

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ make
 sudo make install
 sudo umount mount
 rm base/*
-rm mount/*
 rm log.txt
 ntapfuse mount base mount
 
--- src/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-fusermount -u mountpoint
-sudo make install
-make clean
-rm log.txt
-rm basedir/*
-ntapfuse mount basedir mountpoint
