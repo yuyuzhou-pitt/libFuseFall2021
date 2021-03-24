762e6b73dc1d234483cedea17bc4fbd83f2ae48c
cas380 <cas380@pitt.edu>
2021-03-23 23:35:16 -0400

    log substring check for write_hi

--- tests/write_hi.sh
+++ tests/write_hi.sh
@@ -4,9 +4,18 @@ takedown() {
     rm -rf hello.txt
 }
 
+regex='(write: 
+	PATH: )(\/[a-zA-Z.]*)(
+	SIZE: )([0-9]+)(
+	OFFS: )([0-9]+)'
+
 check_recent_write() {
-    logtext=$(cat ../log.txt)
-    actual="write:${logtext##*$'write:'}"
+    logtext=$(cat ../log.txt) # the entire logs file...
+    recent_logtext="write:${logtext##*$'write:'}" # ...except logs before the most recent write are removed...
+
+    if [[ $recent_logtext =~ $regex ]]; then # ...because bash matches only the first instance of regex
+        actual=${BASH_REMATCH[0]}
+    fi
     
     if [[ $actual != $expected ]]; then
         echo $1
