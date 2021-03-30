492d40940912c4d3d0079695c4d927f3b2898df1
bricktsre <gwoodward21@gmail.com>
2021-03-30 14:02:17 -0400

    adding files

--- test/test_htable.c
+++ test/test_htable.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <time.h>
 #include <sys/types.h>
 
 #include "unity.h"
@@ -14,7 +15,7 @@ htable_enum *he;
 static unsigned int hash_func(const void *in, unsigned int seed)
 {
 	srand(*(unsigned int *)in + seed);
-	return (unsigned int)rand();
+	return *(unsigned int *)in * ((unsigned int)rand())+seed;
 }
 
 static int key_eq(const void *a, const void *b)
@@ -35,6 +36,8 @@ void setUp(void)
 	*default_val = 5678;
 
 	he = htable_enum_create(ht);
+
+	srand(time(NULL));
 }
 
 void tearDown(void)
@@ -45,7 +48,6 @@ void tearDown(void)
 
 void make_hash_pair(uint32_t **key, uint32_t **val)
 {
-	srand(0);
 	*key = (uint32_t *)malloc(sizeof(uint32_t));
 	**key = (uint32_t)rand();
 
@@ -168,4 +170,75 @@ void test_null_get(void)
 {
 	TEST_ASSERT_FALSE(htable_get(NULL, (void *)default_key, NULL));
 	TEST_ASSERT_FALSE(htable_get(ht, NULL, NULL));
+}
+
+void test_update_value(void)
+{
+	htable_insert(ht, (void *)default_key, (void *)default_val);
+	
+	uint32_t *new_val = (uint32_t *)malloc(sizeof(uint32_t));
+	*new_val = 1111;
+	htable_update(ht, (void *)default_key, (void *)new_val);
+
+	uint32_t *return_val;
+	TEST_ASSERT_TRUE(htable_get(ht, (void *)default_key, (void **)&return_val));
+	TEST_ASSERT_EQUAL_INT(*new_val, *return_val);
+	TEST_ASSERT_FALSE(default_val == return_val);
+}
+
+void test_update_in_place(void)
+{
+	htable_insert(ht, (void *)default_key, (void *)default_val);
+	
+	uint32_t *return_val;
+	TEST_ASSERT_TRUE(htable_get(ht, (void *)default_key, (void **)&return_val));
+	
+	*return_val = 5555;
+	htable_update(ht, (void *)default_key, (void *)return_val);
+	
+	uint32_t *return_val_two;
+	TEST_ASSERT_TRUE(htable_get(ht, (void *)default_key, (void **)&return_val_two));
+	TEST_ASSERT_TRUE(return_val_two == default_val);
+}
+
+void test_enum_next(void)
+{
+	htable_insert(ht, (void *)default_key, (void *)default_val);
+	
+	uint32_t *return_key;
+	uint32_t *return_val;
+
+	TEST_ASSERT_TRUE(htable_enum_next(he, (void **)&return_key, (void **)&return_val));
+	TEST_ASSERT_TRUE(*default_key == *return_key);
+	TEST_ASSERT_TRUE(*default_val == *return_val);
+}
+
+void test_enum_next_empty_table(void)
+{
+	TEST_ASSERT_FALSE(htable_enum_next(he, NULL, NULL));
+	TEST_ASSERT_FALSE(htable_enum_next(he, NULL, NULL));
+}
+
+void test_enum_multiple_items(void)
+{
+	uint32_t *key1;
+	uint32_t *key2;
+	uint32_t *val1;
+	uint32_t *val2;
+
+	make_hash_pair(&key1, &val1);
+	make_hash_pair(&key2, &val2);
+
+	htable_insert(ht, (void *)default_key, (void *)default_val);
+	htable_insert(ht, (void *)key1, (void *)val1);
+	htable_insert(ht, (void *)key2, (void *)val2);
+
+	int i = 0;
+	uint32_t *return_val;
+	while(htable_enum_next(he, NULL, (void **)&return_val)) {
+		i++;
+		TEST_ASSERT_TRUE(*return_val == *default_val || *return_val == *val1 || *return_val == *val2);
+		TEST_ASSERT_FALSE(*return_val == *default_val && *return_val == *val1 && *return_val == *val2);
+	}
+	TEST_ASSERT_EQUAL_INT(3, i);
 }
