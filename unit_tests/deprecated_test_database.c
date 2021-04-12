8c2818cf0836a76f9a6688fbbf7ec274c46d5906
Jack Ullery <jmu17@pitt.edu>
2021-04-12 14:03:48 -0400

    pushed test for database that I could not get working

---
+++ unit_tests/deprecated_test_database.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include <limits.h>
+
+#include "unity.h"
+
+#include "database.h"
+#include "mock_htable_record_cache.h"
+#include "mock_common.h"
+// #include "mock_htable_record_cache.h"
+
+void setUp(void)
+{
+	// database_init();
+}
+
+void tearDown(void)
+{
+	// database_close();
+}
